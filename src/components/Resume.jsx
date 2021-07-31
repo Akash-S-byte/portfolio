938c8632af055b18e381c78583a46904024b70db
Akash S <davakash1999@gmail.com>
2021-07-31 20:16:25 +0530

    4157Akash

---
+++ src/components/Resume.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react'
+import './Resume.css'
+export default class Resume extends Component {
+    render() {
+        return (
+                <div class='cont'>
+    <div class='container'>
+        <div class='a'><div class='c'> <div class="d">
+            <h2>S AKASH</h2>
+            <p>B.E – Electronics and Communication Engineeringbr <br/>PSG Institute of Technology and Applied Research, Coimbatore-641062. </p>
+            <div class="b"><strong>E-Mail Id: </strong>davakash1999@gmail.com <br/><strong>Contact No:</strong>9600539192</div></div>
+            <div class='im'><img src="resources/715517106301 (2).jpg" alt="img"/></div></div>
+        </div><hr/>
+    <div class='head'>Personal Details</div><br/>
+    <div class='h1'>
+    <div><strong>Father Name		:</strong>   S P Subbaraj <br/>
+        <strong>Mother Name		    :</strong>   S Karpagajothi <br/>
+        <strong>Gender			    : </strong>  Male <br/> 
+        <strong>Date of Birth		:</strong>   07/09/1999 <br/>
+        <strong>Mother Tongue		:</strong>   Tamil
+    </div>
+<div><strong> PERMANENT ADDRESS </strong><br/>
+    101A, Elango st, Thirumangalam, <br/>
+   Thiruvalleeswarar Nagar, Annanagar West,<br/>
+   Chennai- 600040 
+    </div></div> <br/>
+    
+    <div class='head'>Scholastics</div><br/>
+    <div>
+        <table>
+           <tr>
+               <th>Course</th>
+           <th>Institution</th>
+           <th>Board / University</th>
+           <th>Year of Graduation</th>
+           <th>Aggregate(%) / CGPA</th></tr> 
+           <tr>
+               <th>B.E ECE</th>
+               <td>PSG Institute of Technology and Applied Research, Coimbatore</td>
+               <td>Anna University, Chennai</td>
+               <td>2021</td>
+               <td>7.3</td>
+           </tr>
+           <tr>
+               <th>Diploma in ECE</th>
+               <td>PSG Polytechnic College,Coimbatore</td>
+               <td>Tamilnadu State Board</td>
+               <td>2018</td>
+               <td>89.86%</td>
+           </tr>
+           <tr>
+               <th>X</th>
+               <td>D.A.V  Matriculation Higher Secondary School,Chennai</td>
+               <td>Tamilnadu State Board</td>
+               <td>2015</td>
+               <td>89.8%</td>
+           </tr>
+        </table></div> <br/>
+    
+    <div class='head'>Technical Skills</div><br/>
+    
+    <div><strong> PROGRAMMING LANGUAGES                  : </strong>  C, C++, JAVA, PYTHON, VHDL, SQL, HTML, CSS, JAVA Script <br/>
+
+        <strong>  SIMULATION SOFTWARES                         :</strong>   Ansys HFSS,PSPICE,XILINX
+        </div><br/><div>
+    <div class='head'>Linguistic Skills</div><br/>
+    <div><strong>GENERAL PROFICIENCY	  		: </strong>  English, Tamil, Telugu</div><br/>
+   
+    <div class='head'>Area of Interest</div><br/>
+    <div>•	Programming <br/>
+        •	Digital electronics
+        </div><br/>
+    
+    <div class='head'>Project Works</div><br/>
+    <div>•	Voice controlled car using wi-fi module <br/>
+        •	Home automation using wi-fi module<br/>
+        •	Automatic light dimming in vehicles using relay<br/>
+        •	Capturing images automatically in door lock system <br/>
+        •	Automatic paper cup packing machine <br/>
+        •	Concrete testing using piezo electric sensor  <br/>
+        •	Coal mine inspection using rocker bogie mechanism<br/>
+        •	Line follower robot<br/>
+        </div><br/>
+    
+    <div class='head'>Industry Based Experience</div><br/>
+    <div><table><tr>
+        <th>Name of the Industry</th>
+        <th>Duration</th>
+        <th>Areas of Exposure</th>
+        
+    </tr><tr>
+        <td>Neyveli Lignite Corporation,Neyveli, Tamil Nadu</td>
+        <td>1 week</td>
+        <td>Switching of Calls , Connection of Broadband and optial fiber cable breakage detection</td>
+    </tr>
+    <tr>
+        <td>Vasantha Advanced Systems, Coimbatore, Tamil Nadu</td>
+        <td>5 days</td>
+        <td>PCB designing ,placing components in PCB and soldering</td>
+    </tr>
+    <tr>
+        <td>Radio Hub in PSG College of Technology, Coimbatore, Tamilnadu</td>
+        <td>5 days</td>
+        <td>Transmission of FM Signals and working of radio stations </td>
+    </tr></table></div><br/>
+    <div class='head'>Co-Curricular Activities</div><br/>
+    <div>•	Participated in Workshop on 8085 Microprocessor in PSG Polytechnic College, Coimbatore. <br/>
+        •	Participated in Workshop on FPGA in PSG Institute of Technology and Applied Research, Coimbatore. <br/>
+        •	Participated in Workshop on PIC Microcontroller and TI CC3200 LaunchPad in PSG Institute of Technology and Applied Research, Coimbatore. 
+        </div><br/>
+    <div class='head'>Extra-Curricular Activities</div><br/>
+    <div>•	Served as Joint Secretary in PSG Polytechnic College, Coimbatore. <br/>
+        •	Served as NCC Cadet in School.
+        </div><br/>
+    <div class='head'>Hobby</div><br/>
+    <div>•	Assembling and disassembling of PC and electronic devices. <br/>
+        •	Servicing electric, electronic and mechanical devices like mobile phone, tv, laptop, washing machine, etc. 
+        </div><br/>
+    <div class='head'>Declaration</div><br/>
+    <div>I, S Akash, hereby do confirm that all the details given above are correct, consistent and complete to the best of my knowledge and belief. <br/>
+
+                  							 	                                                
+        (S Akash)
+
+</div>
+    </div></div>
+    </div>
+      )
+    }
+}
